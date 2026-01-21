e923332064f3df7d12fac3b70f077788b37f7de0
nullhandlex <nullhandlex@gmail.com>
2026-01-21 02:51:24 +0200

    creating CQRS interfaces.

--- src/Services/Archive/Archive.API/Albums/CreateAlbum/CreateAlbumHandler.cs
+++ src/Services/Archive/Archive.API/Albums/CreateAlbum/CreateAlbumHandler.cs
@@ -1,6 +1,47 @@
-﻿namespace Archive.API.Albums.CreateAlbum
+﻿using Archive.API.Models;
+using Archive.API.Models.JoinTables;
+using BuildingBlocks.CQRS;
+
+namespace Archive.API.Albums.CreateAlbum
 {
-    public class CreateAlbumHandler
+    public record CreateAlbumCommand(
+        string Title,
+        DateTime ReleaseDate,
+        AlbumType Type,
+        AlbumFormat Format,
+        string? Label,
+        Guid? CountryId,
+        List<Guid> BandIds,
+        List<Guid> GenreIds,
+        List<Guid> TagIds,
+        IFormFile? Cover
+    ) : ICommand<CreateAlbumResult>;
+
+    public record CreateAlbumResult(Guid Id);
+
+    internal class CreateAlbumCommandHandler : ICommandHandler<CreateAlbumCommand, CreateAlbumResult>
     {
+        public Task<CreateAlbumResult> Handle(CreateAlbumCommand command, CancellationToken cancellationToken)
+        {
+            //bussiness logic to create an album
+
+            var album = new Album
+            {
+                Id = Guid.NewGuid(),
+                Title = command.Title,
+                ReleaseDate = command.ReleaseDate,
+                Type = command.Type,
+                Format = command.Format,
+                Label = command.Label,
+                //CountryId = command.CountryId,
+                //CoverUrl = coverUrl,
+                Genres = command.GenreIds.Select(genreId => new AlbumGenre
+                {
+                    GenreId = genreId
+                }).ToList()
+            };
+
+            throw new NotImplementedException();
+        }
     }
 }
