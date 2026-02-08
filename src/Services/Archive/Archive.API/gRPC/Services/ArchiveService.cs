ed41d53c7443b863eaed410c24e0e49a41c3fb26
nullhandlex <oleksandr.medved.n@gmail.com>
2026-02-08 03:42:17 +0200

    Adding basic structure for gRPC in Arhive.API

---
+++ src/Services/Archive/Archive.API/gRPC/Services/ArchiveService.cs
@@ -0,0 +1,20 @@
+﻿using Archive.Grpc;
+using Grpc.Core;
+
+namespace Archive.API.gRPC.Services
+{
+    public class ArchiveService : ArchiveProtoService.ArchiveProtoServiceBase
+    {
+        private readonly ILogger<ArchiveService> _logger;
+
+        public ArchiveService(ILogger<ArchiveService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CheckAlbumExistsResponse> CheckAlbumExists(CheckAlbumExistsRequest request, ServerCallContext context)
+        {
+            return base.CheckAlbumExists(request, context);
+        }
+    }
+}
