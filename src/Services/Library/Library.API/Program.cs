39f91ef1086a75a56bd7ec278d8c9fa4c2768a46
nullhandlex <216790482+nullhandlex@users.noreply.github.com>
2026-04-14 00:45:09 +0300

    FIxing https request with gPRC

--- src/Services/Library/Library.API/Program.cs
+++ src/Services/Library/Library.API/Program.cs
@@ -6,20 +6,6 @@ builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenAnyIP(8080, o => o.Protocols = HttpProtocols.Http1);
     options.ListenAnyIP(8082, o => o.Protocols = HttpProtocols.Http2);
-    options.ListenAnyIP(8081, o =>
-    {
-        o.Protocols = HttpProtocols.Http1AndHttp2;
-        string? certPath = builder.Configuration["Kestrel:Certificates:Default:Path"];
-        if (!string.IsNullOrEmpty(certPath))
-        {
-            string? certPassword = builder.Configuration["Kestrel:Certificates:Default:Password"];
-            o.UseHttps(certPath, certPassword);
-        }
-        else
-        {
-            o.UseHttps();
-        }
-    });
 });
 
 builder.Services
